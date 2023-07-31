64665a4b9f3cd0d969b995e5a546e736db651906
Deepesh Dandriyal <deepeshdandriyal1@gmail.com>
2023-07-31 18:41:42 +0530

    api integrated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 function App() {
+  const [city, setCity] = useState("");
+  const [data, setData] = useState({});
+  const [heading, setHeading] = useState("");
+  const options = {
+    method: "GET",
+    url: "https://air-quality-by-api-ninjas.p.rapidapi.com/v1/airquality",
+    params: { city: `${city}` },
+    headers: {
+      "X-RapidAPI-Key": "4fc9c676damshcc0ced50d54a3afp110c31jsnf1771c758771",
+      "X-RapidAPI-Host": "air-quality-by-api-ninjas.p.rapidapi.com",
+    },
+  };
+  const handleSearch = async (event) => {
+    if (event.key === "Enter") {
+      axios
+        .get(
+          "https://air-quality-by-api-ninjas.p.rapidapi.com/v1/airquality",
+          options
+        )
+        .then((response) => {
+          setData(response.data);
+          setHeading(city);
+          setCity("");
+          console.log(data);
+        });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        width="200px"
+        height="50px"
+        onChange={(event) => setCity(event.target.value)}
+        onKeyDown={handleSearch}
+        value={city}
+        placeholder="Enter City"
+      />
+      {data ? (
+        <div>
+          <h2>{heading}</h2>
+          <h1>{data?.overall_aqi}</h1>
+        </div>
+      ) : (
+        <h1>Enter valid city name</h1>
+      )}
     </div>
   );
 }
